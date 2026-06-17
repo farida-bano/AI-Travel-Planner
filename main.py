31b449bf807afcea5c6738686a8dc8d0eb89d74b
Farida Bano <bano23086@gmaile.com>
2026-06-17 10:37:04 +0500

    Remove streamlit import from main.py - not needed as module

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 from typing import TypedDict, Annotated
 import operator
 
-import streamlit as st
 import psycopg
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import (
